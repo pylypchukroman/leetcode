f32cc4eb22d2645fe30eafe1873d39f3e39c329b
pylypchukroman <romanpylypchuk@gmail.com>
2023-07-11 22:06:40 +0300

    init commit

--- script.js
+++ script.js
@@ -13,3 +13,15 @@
 
 // singleNumber([4,1,2,1,2]);
 
+// Merge Sorted Array (bad solution)
+function merge(nums1, m, nums2, n) {
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let p = m + n - 1;
+
+    while (p2 >= 0) {
+        nums1[p--] = (nums1[p1] > nums2[p2]) ? nums1[p1--] : nums2[p2--];
+    }
+}
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
