375e5b51b094bb629927a6bd1c242a1bd92bca6b
pylypchukroman <romanpylypchuk@gmail.com>
2023-07-11 17:54:52 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// function singleNumber(nums) {
+//     const counter = {};
+//     nums.forEach(num => counter[num] = counter[num] ? counter[num] + 1 : 1);
+//     return +Object.keys(counter).filter(num => counter[num] === 1).join('');
+// }
+
+//*//
+// function singleNumber(nums) {
+//     return nums.reduce((res, num) => res ^ num, 0);
+// }
+//*//
+
+// singleNumber([4,1,2,1,2]);
+
