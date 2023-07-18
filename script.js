f973e32c87329fcff697a70b2f8f9973cb63de2a
pylypchukroman <romanpylypchuk@gmail.com>
2023-07-18 10:06:58 +0300

    new solutions

--- script.js
+++ script.js
@@ -185,3 +185,22 @@ function detectCapitalUse(word) {
 }
 
 detectCapitalUse('FlaG');
+
+//Search Insert Position
+function searchInsert(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+
+searchInsert([1,3,5,6], 2);
