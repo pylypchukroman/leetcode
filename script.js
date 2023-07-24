b4ef348389b6df225137d423e5d7b4450ac80ce7
pylypchukroman <romanpylypchuk@gmail.com>
2023-07-24 19:03:31 +0300

    new solutions

--- script.js
+++ script.js
@@ -426,3 +426,29 @@ function maximumProduct(nums) {
 }
 
 maximumProduct([-1,-2,-3]);
+
+//Find Smallest Letter Greater Than Target
+function nextGreatestLetter(letters, target) {
+    if (letters.length === 1) {
+        return letters[0];
+    }
+
+    let left = 0;
+    let right = letters.length;
+
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (letters[mid] <= target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    if (left === letters.length) {
+        return letters[0];
+    } else {
+        return letters[left];
+    }
+}
+
+nextGreatestLetter(["c","f","j"], 'a');
