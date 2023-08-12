b185e5a0bc1c77146c3fdc556c3967c792986adb
pylypchukroman <romanpylypchuk@gmail.com>
2023-08-12 17:42:21 +0300

    new solutions

--- script.js
+++ script.js
@@ -1154,3 +1154,15 @@ function reformatDate(date) {
 }
 
 reformatDate('26th May 1960');
+
+//Three Consecutive Odds
+function threeConsecutiveOdds(arr) {
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0 && arr[i + 2] % 2 !== 0  ) {
+            return true;
+        }
+    }
+    return false;
+}
+
+threeConsecutiveOdds([1,2,34,3,4,5,7,23,12]);
