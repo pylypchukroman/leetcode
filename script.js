35c3aabc6e9b23511c268888db2e7bff809664be
pylypchukroman <romanpylypchuk@gmail.com>
2023-08-18 11:21:40 +0300

    new solutions

--- script.js
+++ script.js
@@ -1405,4 +1405,18 @@ function removeElement (nums, val) {
 
 removeElement([3,2,2,3], 3);
 
+//Check If N and Its Double Exist
+function checkIfExist (arr) {
+    for (let i= 0; i < arr.length; i++) {
+        for (let j= 0; j<arr.length; j++) {
+            if (i !== j && arr[i] === 2 * arr[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+checkIfExist([-10,12,-20,-8,15]);
+
 
