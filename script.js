8d99ac8d03d9a9567e78f7ffb207a6bd49328dd8
pylypchukroman <romanpylypchuk@gmail.com>
2023-08-25 07:33:21 +0300

    new solutions

--- script.js
+++ script.js
@@ -1760,3 +1760,19 @@ function canBeTypedWords (text, brokenLetters) {
 }
 
 canBeTypedWords('leet code', 'lt');
+
+//Count Number of Pairs With Absolute Difference K
+function countKDifference (nums, k) {
+    nums.sort((a, b) => a - b);
+    let result = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (Math.abs(nums[i] - nums[j]) === k) {
+                result++;
+            }
+        }
+    }
+    return result;
+}
+
+countKDifference([1,2,2,1], 1);
