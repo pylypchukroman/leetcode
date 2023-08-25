9cd3a205aa13696577f1b2ea50428c1610e439e7
pylypchukroman <romanpylypchuk@gmail.com>
2023-08-25 11:04:43 +0300

    new solutions

--- script.js
+++ script.js
@@ -1794,3 +1794,17 @@ function repeatedNTimes (nums) {
 }
 
 repeatedNTimes([1,2,3,3]);
+
+//Delete Characters to Make Fancy String
+function makeFancyString (s) {
+    const arr = s.split('');
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1] || arr[i] !== arr[i + 2]) {
+            result.push(arr[i]);
+        }
+    }
+    return result.join('');
+}
+
+makeFancyString('leeetcode');
