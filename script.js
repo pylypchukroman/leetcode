234f0627738a9f71e728ddef6f43a7e7a8f60bbe
pylypchukroman <romanpylypchuk@gmail.com>
2023-08-30 21:52:51 +0300

    new solutions

--- script.js
+++ script.js
@@ -2048,3 +2048,22 @@ function canMakeArithmeticProgression (arr) {
 }
 
 canMakeArithmeticProgression([3,5,1]);
+
+//Minimum Absolute Difference
+function minimumAbsDifference (arr) {
+    let min = Number.MAX_VALUE;
+    let result = [];
+    arr = arr.sort((a, b) => a - b);
+    for (let i = 1; i < arr.length; i++){
+        let diff = arr[i] - arr[i-1];
+        if (diff < min) {
+            min = diff;
+            result = [[arr[i-1], arr[i]]];
+        } else if (diff === min) {
+            result.push([arr[i-1], arr[i]]);
+        }
+    }
+    return result;
+}
+
+minimumAbsDifference([1,3,6,10,15]);
