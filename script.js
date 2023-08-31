cc1a7bc351ecd4cb9c550b4c338459f1b3f2d175
pylypchukroman <romanpylypchuk@gmail.com>
2023-08-31 17:08:18 +0300

    new solutions

--- script.js
+++ script.js
@@ -2098,3 +2098,13 @@ function kthDistinct (arr, k) {
 }
 
 kthDistinct(["a","b","a"], 3);
+
+//
+function smallestEqual (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 10 === nums[i]) return i;
+    }
+    return -1;
+}
+
+smallestEqual([1,2,3,4,5,6,7,8,9,0]);
