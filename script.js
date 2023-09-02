22605074e602cd7c880de65b974e140eae8f0d4a
pylypchukroman <romanpylypchuk@gmail.com>
2023-09-02 18:35:31 +0300

    new solutions

--- script.js
+++ script.js
@@ -2163,3 +2163,18 @@ function mergeAlternately (word1, word2) {
 }
 
 mergeAlternately('ab', 'pqrs');
+
+//Number of Good Pairs
+function numIdenticalPairs (nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j ++) {
+            if (nums[i] === nums[j]) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+numIdenticalPairs([1,2,3,1,1,3]);
