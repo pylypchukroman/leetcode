64aaf46b8f320d446847794d102ce99cd25a3f21
pylypchukroman <romanpylypchuk@gmail.com>
2023-09-04 18:00:12 +0300

    new solutions

--- script.js
+++ script.js
@@ -2250,3 +2250,20 @@ function maximumNumberOfStringPairs (words) {
 }
 
 maximumNumberOfStringPairs(["cd","ac","dc","ca","zz"]);
+
+//Count Equal and Divisible Pairs in an Array
+function countPairs (nums, k) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                if ((i * j) % k === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+countPairs([3,1,2,2,2,1,3], 2);
