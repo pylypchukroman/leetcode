45ec8a53c41f640d73ea789acc23ce1b99ed092e
pylypchukroman <romanpylypchuk@gmail.com>
2023-09-27 16:09:01 +0300

    new solutions

--- script.js
+++ script.js
@@ -2749,3 +2749,18 @@ function theMaximumAchievableX(num, t) {
 }
 
 theMaximumAchievableX(4, 1);
+
+//Count Pairs Whose Sum is Less than Target
+function countPairs(nums, target) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+countPairs([-6,2,5,-2,-7,-1,3], -2);
