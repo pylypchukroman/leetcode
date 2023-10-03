d00506e6ec7d6da73be889052beac51b09577870
pylypchukroman <romanpylypchuk@gmail.com>
2023-10-03 17:07:23 +0300

    new solutions

--- script.js
+++ script.js
@@ -3016,3 +3016,10 @@ function deleteGreatestValue(grid) {
 
 deleteGreatestValue([[1,2,4],[3,3,1]]);
 
+//Determine Color of a Chessboard Square
+function squareIsWhite(coordinates) {
+    const whiteSquares = ['a2', 'a4', 'a6', 'a8', 'b1', 'b3', 'b5', 'b7', 'c2', 'c4', 'c6', 'c8', 'd1', 'd3', 'd5', 'd7', 'e2', 'e4', 'e6', 'e8', 'f1', 'f3', 'f5', 'f7', 'g2', 'g4', 'g6', 'g8', 'h1', 'h3', 'h5', 'h7'];
+    return whiteSquares.includes(coordinates);
+}
+
+squareIsWhite('a1');
