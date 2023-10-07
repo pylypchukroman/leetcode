8a5058690e28826345a9366e22350e2cfea7e998
pylypchukroman <romanpylypchuk@gmail.com>
2023-10-07 15:36:46 +0300

    new solutions

--- script.js
+++ script.js
@@ -3075,4 +3075,19 @@ function distinctDifferenceArray(nums) {
 
 distinctDifferenceArray([1,2,3,4,5]);
 
+//Maximum Number of Pairs in Array
+    function numberOfPairs(nums) {
+    let count = 0;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (nums[i] === nums[j]) {
+                        count++;
+                        nums.splice(i, 1);
+                        nums.splice(j, 1);
+            }
+        }
+    }
+    return [count, nums.length];
+    }
 
+numberOfPairs([1,3,2,1,3,2,2]);
