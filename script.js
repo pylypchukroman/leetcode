504180912aca844b74188da8143e27d4c605849f
pylypchukroman <romanpylypchuk@gmail.com>
2023-10-16 16:20:28 +0300

    new solutions

--- script.js
+++ script.js
@@ -3258,3 +3258,11 @@ function sortArrayByParityII(nums) {
 }
 
 sortArrayByParityII([4,2,5,7]);
+
+//Fibonacci Number
+function fib(n) {
+    if (n === 0 || n === 1) return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+fib(2);
