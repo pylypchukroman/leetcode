05796ca90bafef111e74562a68d3317f9b38d810
pylypchukroman <romanpylypchuk@gmail.com>
2023-10-23 18:31:42 +0300

    new solutions

--- script.js
+++ script.js
@@ -3505,3 +3505,20 @@ function shiftGrid(grid, k) {
 }
 
 shiftGrid([[1,2,3],[4,5,6],[7,8,9]], 1);
+
+//Count Square Sum Triples
+function countTriples(n) {
+    let count = 0;
+    for (let i = 0; i <= n - 2; i++) {
+        for (let j = i + 1; j <= n - 1; j++) {
+            for (let k = j + 1; k <= n; k++) {
+                if (Math.pow(i, 2) + Math.pow(j, 2) === Math.pow(k, 2)) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count * 2;
+}
+
+countTriples(10);
