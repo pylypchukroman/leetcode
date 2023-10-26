d484407f8f8dea6b4bbce4864f45b82c70e9ca0b
pylypchukroman <romanpylypchuk@gmail.com>
2023-10-26 18:15:22 +0300

    new solutions

--- script.js
+++ script.js
@@ -3631,3 +3631,17 @@ function numberOfLines(widths, s) {
 }
 
 numberOfLines([10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], 'abcdefghijklmnopqrstuvwxyz');
+
+//Mean of Array After Removing Some Elements
+function trimMean(arr) {
+    arr.sort((a, b) => a - b);
+    let p = arr.length * 0.05;
+    let sum = 0;
+    for (let i = p; i < arr.length - p; i++) {
+        sum += arr[i];
+    }
+
+    return sum / (arr.length - (p * 2));
+}
+
+trimMean([6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4]);
