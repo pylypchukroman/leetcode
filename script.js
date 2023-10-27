423a7af5ce97055737551d120aaded2af0539afd
pylypchukroman <romanpylypchuk@gmail.com>
2023-10-27 19:06:30 +0300

    new solutions

--- script.js
+++ script.js
@@ -3645,3 +3645,10 @@ function trimMean(arr) {
 }
 
 trimMean([6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4]);
+
+//Distribute Candies
+function distributeCandies(candyType) {
+    return Math.min(new Set(candyType).size, candyType.length / 2);
+}
+
+distributeCandies([1,1,2,2,3,3]);
