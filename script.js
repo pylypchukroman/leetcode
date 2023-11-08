ac2a3a897537f0bfe449eeca111221b9330784c9
pylypchukroman <romanpylypchuk@gmail.com>
2023-11-08 21:19:59 +0200

    new solutions

--- script.js
+++ script.js
@@ -3836,3 +3836,16 @@ function isIsomorphic(s, t) {
 }
 
 isIsomorphic('paper', 'title');
+
+//Rotate Array
+function rotate(nums, k) {
+    for (let i = nums.length - 1; i >= 0; i--) {
+        nums[i + k] = nums[i];
+    }
+    for (let j = k - 1; j >= 0; j--) {
+        nums[j] = nums.pop();
+    }
+    return nums;
+}
+
+rotate([1,2,3,4,5,6,7], 3);
