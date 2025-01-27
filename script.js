d26fbfd121b8a0f8be8afeb4215a00214b13725a
pylypchukroman <romanpylypchuk@gmail.com>
2025-01-27 19:15:05 +1100

    new solutions

--- script.js
+++ script.js
@@ -3922,3 +3922,9 @@ const lengthOfLastWord = function(s) {
 };
 
 lengthOfLastWord('Hello World');
+
+const plusOne = function(digits) {
+    return (BigInt(digits.join("")) + BigInt(1)).toString().split("").map(i=>Number(i))
+};
+
+plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]);
