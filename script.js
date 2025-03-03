bbecd76321804c88b4b40462cbd7f43f69ffbfe2
pylypchukroman <romanpylypchuk@gmail.com>
2025-03-03 19:31:29 +1100

    new solutions

--- script.js
+++ script.js
@@ -3928,3 +3928,13 @@ const plusOne = function(digits) {
 };
 
 plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]);
+
+//Single Number
+const singleNumber1 = function(nums) {
+    const obj = {};
+    nums.forEach(num => obj[num] = obj[num] ? obj[num] + 1 : 1);
+    const index = Object.values(obj).findIndex(x => x === 1);
+    return +Object.keys(obj)[index];
+};
+
+singleNumber1([4,1,2,1,2]);
