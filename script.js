8d0b8f40c811452e16f0b0bfe38212d2925e43c6
pylypchukroman <romanpylypchuk@gmail.com>
2025-03-31 21:20:21 +1100

    new solutions

--- script.js
+++ script.js
@@ -4102,3 +4102,23 @@ const groupAnagrams3 = function(strs) {
     }
     return [...map.values()];
 };
+
+const fizzBuzz2 = function(n) {
+    const result = [];
+    for(let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0)
+        {
+            result[i - 1] = 'FizzBuzz';
+        } else if (i % 3 === 0)
+        {
+            result[i - 1] = 'Fizz';
+        } else if (i % 5 === 0)
+        {
+            result[i - 1] = 'Buzz';
+        } else
+        {
+            result[i - 1] ='' + i;
+        }
+    }
+    return result;
+};
