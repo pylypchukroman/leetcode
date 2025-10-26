6c2fe81a1b817dfd640d48096a93e6b46f2afbf6
pylypchukroman <romanpylypchuk@gmail.com>
2025-10-26 17:20:22 +1100

    new solutions

--- script.js
+++ script.js
@@ -4248,3 +4248,25 @@ const longestCommonPrefixNew = function(strs) {
 };
 
 // longestCommonPrefixNew(["flower","flow","flight"]);
+
+////binary search
+
+const searchNew = function(nums, target) {
+    let leftIndex = 0;
+    let rightIndex = nums.length - 1;
+    while (leftIndex <= rightIndex) {
+        let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+        console.log(middleIndex, 'index')
+        if (nums[middleIndex] === target) {
+            return middleIndex;
+        }
+        if (target < nums[middleIndex]) {
+            rightIndex = middleIndex - 1;
+        } else {
+            leftIndex = middleIndex + 1;
+        }
+    }
+    return -1;
+};
+
+// console.log(searchNew([5], 5));
