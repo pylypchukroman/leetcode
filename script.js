09031a366962d968e739d33a7aa2c3aadbb80f0f
pylypchukroman <romanpylypchuk@gmail.com>
2025-10-27 20:24:43 +1100

    new solutions

--- script.js
+++ script.js
@@ -4290,3 +4290,15 @@ const searchInsertNew = function(nums, target) {
 };
 
 // console.log(searchInsertNew([1,3,5,6], 7));
+
+//268. Missing Number
+const missingNumberNew = function(nums) {
+    const sorted = nums.sort((a, b) => a - b);
+    for (let i = 0; i < sorted.length + 1; i++) {
+        if (sorted[i] !== i) {
+            return i;
+        }
+    }
+};
+
+// console.log(missingNumberNew([45,35,38,13,12,23,48,15,44,21,43,26,6,37,1,19,22,3,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14]));
