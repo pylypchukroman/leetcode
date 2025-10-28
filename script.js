a576ad21a0e06666ddde499ceedab05c5d458dd4
pylypchukroman <romanpylypchuk@gmail.com>
2025-10-28 17:45:48 +1100

    new solutions

--- script.js
+++ script.js
@@ -4302,3 +4302,15 @@ const missingNumberNew = function(nums) {
 };
 
 // console.log(missingNumberNew([45,35,38,13,12,23,48,15,44,21,43,26,6,37,1,19,22,3,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14]));
+
+//Intersection of Two Arrays
+
+const intersectionNew = function(nums1, nums2) {
+    const result = new Set();
+    for (let num of nums1) {
+        if (nums2.includes(num)) {
+            result.add(num);
+        }
+    }
+    return [...result];
+};
