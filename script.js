2a549689a6d3c1194a18fd425bcef5a587b66f06
pylypchukroman <romanpylypchuk@gmail.com>
2025-11-17 20:23:44 +1100

    new solutions

--- script.js
+++ script.js
@@ -4499,3 +4499,14 @@ const isSubsequence = function(s, t) {
 
 };
 // console.log(isSubsequence('abc', 'ahbgdc'))
+//657. Robot Return to Origin
+const judgeCircleNew = function(moves) {
+    let l = 0, r = 0, u = 0, d = 0;
+    for (let move of moves) {
+        if (move === 'L') l++;
+        else if (move === 'R') r++;
+        else if (move === 'U') u++;
+        else if (move === 'D') d++;
+    }
+    return l === r && u === d;
+};
