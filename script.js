4c24edbb23cb56e68b26621844a660913878592f
pylypchukroman <romanpylypchuk@gmail.com>
2025-11-25 19:07:39 +1100

    new solutions

--- script.js
+++ script.js
@@ -4522,3 +4522,11 @@ const numJewelsInStonesII = function(jewels, stones) {
     }
     return cnt
 };
+
+//88. Merge Sorted Array
+const mergeII = function(nums1, m, nums2, n) {
+    for (let i = m, j = 0; j < n; i++, j++) {
+        nums1[i] = nums2[j];
+    }
+    nums1.sort((a, b) => a - b);
+};
