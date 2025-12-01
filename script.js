df18533617fdbb31e9953b8cfe48f944eecd5899
pylypchukroman <romanpylypchuk@gmail.com>
2025-12-01 21:00:09 +1100

    new solutions

--- script.js
+++ script.js
@@ -4530,3 +4530,13 @@ const mergeII = function(nums1, m, nums2, n) {
     }
     nums1.sort((a, b) => a - b);
 };
+//383. Ransom Note
+const canConstructII = function(ransomNote, magazine) {
+    if (ransomNote.length > magazine.length) return false;
+    for (let i = 0; i < ransomNote.length; i++) {
+        let char = ransomNote[i];
+        if (!magazine.includes(char)) return false;
+        magazine = magazine.replace(char, '');
+    }
+    return true;
+};
