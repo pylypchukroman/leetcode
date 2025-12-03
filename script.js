f82cefeadb406aaae185092cfa2a9596d633ce86
pylypchukroman <romanpylypchuk@gmail.com>
2025-12-03 21:05:28 +1100

    new solutions

--- script.js
+++ script.js
@@ -4540,3 +4540,14 @@ const canConstructII = function(ransomNote, magazine) {
     }
     return true;
 };
+//605. Can Place Flowers
+const canPlaceFlowersII = function(flowerbed, n) {
+    let count = 0;
+    for(let i = 0; i < flowerbed.length; i++){
+        if(!flowerbed[i - 1] && !flowerbed[i + 1] && !flowerbed[i]) {
+            flowerbed[i] = 1;
+            count++;
+        }
+    }
+    return count >= n;
+};
